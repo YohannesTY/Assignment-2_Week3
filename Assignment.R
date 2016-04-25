8dd9e869f432c4add0467bc8751fcdbb88370b48
YohannesTY <tadesseyohannes@yahoo.com>
2016-04-24 19:54:24 -0500

    Added files via upload

---
+++ Assignment.R
@@ -0,0 +1,32 @@
+# Date: April 24, 2016
+# These pair of functions cache the inverse of a matrix
+#
+
+
+
+makeCacheMatrix <- function(x = matrix()) {
+    I <- NULL #create a place holder 
+    set <- function(y) {
+        x <<- y
+        I <<- NULL # resets I
+    }
+    get <- function() x
+    setinverse <- function(inverse) I <<- inverse
+    getinverse <- function() I
+    list(set=set, 
+         get=get, 
+         setinverse=setinverse, 
+         getinverse=getinverse)
+}
+
+cacheSolve <- function(x, ...) {
+    I <- x$getinverse()
+    if(!is.null(I)) {
+        message("getting cached data")        
+        return(I)
+    }
+    data <- x$get()
+    I <- solve(data)
+    x$setinverse(I)
+    I
+}
